a00f3c4bcc8139972d43d7a27c4387e4034d9469
Kilar Pafifov <apafifova@list.ru>
2018-07-19 16:34:15 +0300

    add button Export CSV

--- Nevlabs/Form1.Designer.cs
+++ Nevlabs/Form1.Designer.cs
@@ -33,12 +33,15 @@
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -78,6 +81,25 @@
             this.dataGridView1.Size = new System.Drawing.Size(681, 280);
             this.dataGridView1.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(694, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 48);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(461, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Нажмите кнопку \"Export\" , чтобы отобразить данные из БД ";
+            // 
             // name
             // 
             this.name.HeaderText = "name";
@@ -98,30 +120,46 @@
             this.phone.HeaderText = "phone";
             this.phone.Name = "phone";
             // 
-            // button2
+            // radioButton1
             // 
-            this.button2.Location = new System.Drawing.Point(694, 279);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 48);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Export";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(857, 200);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 24);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "by name";
+            this.radioButton1.UseVisualStyleBackColor = true;
             // 
-            // label2
+            // radioButton2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(149, 119);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(461, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Нажмите кнопку \"Export\" , чтобы отобразить данные из БД ";
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(857, 303);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(86, 24);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "by date";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(694, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 47);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Export csv";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(976, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
@@ -142,12 +180,15 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
         private System.Windows.Forms.DataGridViewTextBoxColumn date;
         private System.Windows.Forms.DataGridViewTextBoxColumn email;
         private System.Windows.Forms.DataGridViewTextBoxColumn phone;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Nevlabs/Form1.cs
+++ Nevlabs/Form1.cs
@@ -18,6 +18,7 @@ namespace Nevlabs
         {
             InitializeComponent();
             button1.Click += button1_Click;
+            button3.Click += button3_Click;
             openFileDialog1.Filter = "Text files(*.scv)|*.csv|All files(*.*)|*.*";
         }
         
@@ -39,16 +40,14 @@ namespace Nevlabs
                 buf = elem.Split('\t');
                 Profile profile = new Profile(buf[0], buf[1], buf[2], buf[3]);
                 list.Add(profile);
-
             }
 
             foreach(Profile elem in list)
             {
-
                 SqlCommand insertProfiles = new SqlCommand("INSERT INTO [Profiles] " +
                "(Name, DateOfBirth, Email, PhoneNumber) VALUES (@Name, @DateOfBirth, @Email, @PhoneNumber)"
                , connection);
-
+                
                 insertProfiles.Parameters.AddWithValue("Name", elem.name);
                 insertProfiles.Parameters.AddWithValue("DateOfBirth", elem.date);
                 insertProfiles.Parameters.AddWithValue("Email", elem.email);
@@ -88,29 +87,87 @@ namespace Nevlabs
 
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
-            string query = "SELECT * FROM Profiles";
+            string query = "SELECT name, dateOfBirth, email, phoneNumber FROM Profiles";
+
+            if (radioButton1.Checked)
+            {
+                 query += " order by name";
+            }
+
+            else if (radioButton2.Checked)
+            {
+                query += " order by dateOfBirth";
+            }
+
             SqlCommand outputProfiles = new SqlCommand(query, connection);
-
             SqlDataReader reader = outputProfiles.ExecuteReader();
-            List<string[]> data = new List<string[]>();
-
+            
+            string[] parseredRows = new string[reader.FieldCount];
+            
             while (reader.Read())
             {
-                data.Add(new string[4]);
-
-                data[data.Count - 1][0] = reader[0].ToString();
-                data[data.Count - 1][1] = reader[1].ToString();
-                data[data.Count - 1][2] = reader[2].ToString();
-                data[data.Count - 1][3] = reader[3].ToString();
-            }
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    parseredRows[i] = reader[i].ToString();
+                }
+                
+                dataGridView1.Rows.Add(parseredRows);   
+            }    
 
             reader.Close();
             connection.Close();
+        }
 
-            foreach(string[] elem in data)
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\"
+            + @"килар\source\repos\Nevlabs\Nevlabs\Database1.mdf;Integrated Security=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+
+            SqlCommand outputProfiles = new SqlCommand(" SELECT * FROM Profiles "
+                , connection);
+            SqlDataReader reader = outputProfiles.ExecuteReader();
+
+            List<string> list = new List<string>();
+            
+            while (reader.Read())
             {
-                dataGridView1.Rows.Add(elem);
+                string[] stringsForReader = new string[reader.FieldCount];
+                string stringsForList = "";
+
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    stringsForReader[i] = reader[i].ToString();
+                }
+
+                for(int i = 0; i < reader.FieldCount; i++)
+                {
+                    if (i == reader.FieldCount - 1)
+                    {
+                        stringsForList += stringsForReader[i];
+                    }
+                    else
+                    {
+                        stringsForList += stringsForReader[i] + '\t';
+                    }
+                }
+
+                list.Add(stringsForList);
             }
+
+            reader.Close();
+
+            string filename = openFileDialog1.FileName;
+            File.WriteAllLines(Path.GetFullPath(filename), list);
+
+            connection.Close();
+            MessageBox.Show("Данные успешно экспортированы в файл CSV ");
         }
     }
 }
